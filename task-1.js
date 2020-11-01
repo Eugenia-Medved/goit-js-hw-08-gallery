6d71351c3d03f59473849c38f6f5516b11edd3a8
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2020-11-01 19:26:15 +0200

    HW 8 part1

---
+++ task-1.js
@@ -0,0 +1,47 @@
+import galleryItems from "./gallery-items.js";
+
+const galleryListEl = document.querySelector(".js-gallery");
+
+const makeImgCard = ({ preview, original, description }) => {
+  const galleryCartEl = document.createElement("li");
+  galleryCartEl.classList.add("gallery__item");
+
+  const galeryImgEl = document.createElement("img");
+  galeryImgEl.classList.add("gallery__image");
+  galeryImgEl.src = preview;
+  galeryImgEl.dataset.source = original;
+  galeryImgEl.alt = description;
+
+  galleryCartEl.append(galeryImgEl);
+
+  return galleryCartEl;
+};
+
+const galleryItem = galleryItems.map(makeImgCard);
+galleryListEl.append(...galleryItem);
+
+//Modal open/close
+
+const modalEl = document.querySelector(".js-lightbox");
+const imgBigEl = document.querySelector(".lightbox__image");
+
+const modalBtnEl = document.querySelector(".lightbox__button");
+
+galleryListEl.addEventListener("click", onOpenModal);
+modalBtnEl.addEventListener("click", onCloseModal);
+
+function onPaletteContainerClick(evt) {
+  const isColorSwatchEl = evt.target.classList.contains("color-swatch");
+}
+
+function onOpenModal(evt) {
+  imgBigEl.src = evt.target.dataset.source;
+  imgBigEl.alt = evt.target.alt;
+  modalEl.classList.add("is-open");
+}
+
+function onCloseModal() {
+  imgBigEl.removeAttribute("src");
+  imgBigEl.removeAttribute("alt");
+  modalEl.classList.remove("is-open");
+}
