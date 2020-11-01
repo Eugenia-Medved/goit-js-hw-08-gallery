02cbc50d1882d1b71421db3c1df4585f37d39233
YevheniiaBondarenko <65921369+YevheniiaBondarenko@users.noreply.github.com>
2020-11-01 19:44:24 +0200

    HW 8 fix1

--- task-1.js
+++ task-1.js
@@ -6,13 +6,18 @@ const makeImgCard = ({ preview, original, description }) => {
   const galleryCartEl = document.createElement("li");
   galleryCartEl.classList.add("gallery__item");
 
+  const galerylinkEl = document.createElement("a");
+  galerylinkEl.classList.add("gallery__link");
+  galerylinkEl.href = original;
+
   const galeryImgEl = document.createElement("img");
   galeryImgEl.classList.add("gallery__image");
   galeryImgEl.src = preview;
   galeryImgEl.dataset.source = original;
   galeryImgEl.alt = description;
 
-  galleryCartEl.append(galeryImgEl);
+  galerylinkEl.append(galeryImgEl);
+  galleryCartEl.append(galerylinkEl);
 
   return galleryCartEl;
 };
@@ -28,6 +33,7 @@ galleryListEl.addEventListener("click", onOpenModal);
 modalBtnEl.addEventListener("click", onCloseModal);
 
 function onOpenModal(evt) {
+  evt.preventDefault();
   imgBigEl.src = evt.target.dataset.source;
   imgBigEl.alt = evt.target.alt;
   modalEl.classList.add("is-open");
